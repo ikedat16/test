0eb5f980bbfd38541677420854e95eec6ea66ecd
LAPTOP-SALD27AK\tomokazu ikeda <uninterruptedrests@gmail.com>
2017-03-05 21:27:06 +0900

    新規

---
+++ JUnitTest/src/com/ne/jp/Hige.java
@@ -0,0 +1,13 @@
+package com.ne.jp;
+
+public class Hige {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+	public int count(int i, int i2) {
+		return i + i2;
+	}
+
+}
---
+++ JUnitTest/test/com/ne/jp/HigeTest.java
@@ -0,0 +1,34 @@
+package com.ne.jp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HigeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCount() {
+		fail("‚Ü‚¾ŽÀ‘•‚³‚ê‚Ä‚¢‚Ü‚¹‚ñ");
+	}
+
+}
