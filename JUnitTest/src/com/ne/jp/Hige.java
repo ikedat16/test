fcc3fa46399def92792e175e4a414d0217d57b1a
LAPTOP-SALD27AK\tomokazu ikeda <uninterruptedrests@gmail.com>
2017-03-15 00:52:16 +0900

    add optional

--- JUnitTest/src/com/ne/jp/Hige.java
+++ JUnitTest/src/com/ne/jp/Hige.java
@@ -1,12 +1,16 @@
 package com.ne.jp;
 
 import java.io.File;
+import java.util.Optional;
 import java.util.function.Function;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
 
 public class Hige {
 
 	public static void main(String[] args) {
-		new Hige().count(2, 2);
+		new Hige().optional(Optional.ofNullable("abc"));
+//		new Hige().count(2, 2);
 	}
 	public int count(int i, int i2) {
 		aa(a -> a.length());
