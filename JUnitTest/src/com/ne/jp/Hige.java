76be7cd72411eb5ccc83e7493ca8a85e3160a637
ikedat16 <uninterruptedrests@gmail.com>
2017-03-15 00:49:15 +0900

    mod2

--- JUnitTest/src/com/ne/jp/Hige.java
+++ JUnitTest/src/com/ne/jp/Hige.java
@@ -18,6 +18,6 @@ public class Hige {
 		File file = f.apply("test2.txt");
 		System.out.println(file);
 		System.out.println(fa.apply("teststr"));
-		System.out.println("master mod1");
+		System.out.println("master mod2");
 	}
 }
